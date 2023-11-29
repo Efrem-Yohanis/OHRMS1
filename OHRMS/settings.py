295e66a29bf72cf481dc5b6e190588c2d7151f36
Efrem-Yohanis-Anbese <efremyohanis116@gmail.com>
2023-11-29 22:00:19 +0300

    deploye

--- OHRMS/settings.py
+++ OHRMS/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0)zsyrk(i8nc67lrmlw=qzqa2nlf(ka+61c-o9g@9!)dv=&taj'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+# 'django-insecure-0)zsyrk(i8nc67lrmlw=qzqa2nlf(ka+61c-o9g@9!)dv=&taj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG","True").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -72,16 +74,26 @@ WSGI_APPLICATION = 'OHRMS.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ohrmsdb',
+        'USER': 'ohrmsdb_user',
+        'PASSWORD': 'rYCzLOMytQXtRhQ0rAneqbXUa3XptJR4',
+        'HOST': 'dpg-cljnhe98mmjc73da4kj0-a.oregon-postgres.render.com',
+        'PORT': '',
     }
 }
+database_url = os.environ.get("DATABASES_URL")
+# Parse the PostgreSQL database URL
+url = "postgres://ohrmsdb_user:rYCzLOMytQXtRhQ0rAneqbXUa3XptJR4@dpg-cljnhe98mmjc73da4kj0-a.oregon-postgres.render.com/ohrmsdb"
+parsed_db = dj_database_url.parse(database_url)
 
+# Update the 'default' database configuration
 
+DATABASES['default'].update(parsed_db)
 # Password validation
+
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
