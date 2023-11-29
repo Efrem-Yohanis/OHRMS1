0e9bccdf18798e69d31200d6b63e03395605e092
Efrem-Yohanis-Anbese <efremyohanis116@gmail.com>
2023-11-29 23:43:29 +0300

    deploye

--- OHRMS/urls.py
+++ OHRMS/urls.py
@@ -18,9 +18,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + staticfiles_urlpatterns
 
