4f562bb37c462bb3e0cc500faabb2d06f520d82b
Efrem-Yohanis-Anbese <efremyohanis116@gmail.com>
2023-11-30 00:13:47 +0300

    deploye

--- OHRMS/settings.py
+++ OHRMS/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware"
 ]
 
 ROOT_URLCONF = 'OHRMS.urls'
@@ -130,8 +131,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS=[BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "assets"
+STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
