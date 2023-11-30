f238a23372d26b0dda51d6935c168ac64857f5e8
Efrem-Yohanis-Anbese <efremyohanis116@gmail.com>
2023-11-30 23:10:34 +0300

    add login page new

--- OHRMS/settings.py
+++ OHRMS/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
-# 'django-insecure-0)zsyrk(i8nc67lrmlw=qzqa2nlf(ka+61c-o9g@9!)dv=&taj'
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-0)zsyrk(i8nc67lrmlw=qzqa2nlf(ka+61c-o9g@9!)dv=&taj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG","True").lower() == "true"
-
+# DEBUG = os.environ.get("DEBUG","True").lower() == "true"
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
