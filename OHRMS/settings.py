39a94e4458f359420fc385c0957f3d7a2da1d757
Efrem-Yohanis-Anbese <efremyohanis116@gmail.com>
2023-11-30 21:36:27 +0300

    deploye

--- OHRMS/settings.py
+++ OHRMS/settings.py
@@ -130,10 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATIC_URL = 'static/'
-STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / "static"] # new
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- OHRMS/urls.py
+++ OHRMS/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +27,4 @@ urlpatterns = [
 ] 
 
 urlpatterns += staticfiles_urlpatterns()
+
