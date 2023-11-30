2cd1be9b5bc6efb0a1b00183e87532e3b2978a75
Efrem-Yohanis-Anbese <efremyohanis116@gmail.com>
2023-11-30 21:43:14 +0300

    deploye

--- OHRMS/settings.py
+++ OHRMS/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","True").lower() == "true"
 
-# ALLOWED_HOSTS = []
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -86,14 +86,14 @@ DATABASES = {
         'PORT': '',
     }
 }
-database_url = os.environ.get("DATABASES_URL")
-# Parse the PostgreSQL database URL
-url = "postgres://ohrmsdb_user:rYCzLOMytQXtRhQ0rAneqbXUa3XptJR4@dpg-cljnhe98mmjc73da4kj0-a.oregon-postgres.render.com/ohrmsdb"
-parsed_db = dj_database_url.parse(database_url)
+# database_url = os.environ.get("DATABASES_URL")
+# # Parse the PostgreSQL database URL
+# url = "postgres://ohrmsdb_user:rYCzLOMytQXtRhQ0rAneqbXUa3XptJR4@dpg-cljnhe98mmjc73da4kj0-a.oregon-postgres.render.com/ohrmsdb"
+# parsed_db = dj_database_url.parse(database_url)
 
-# Update the 'default' database configuration
+# # Update the 'default' database configuration
 
-DATABASES['default'].update(parsed_db)
+# DATABASES['default'].update(parsed_db)
 # Password validation
 
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
